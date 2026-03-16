921fd893bb394ecdc5896f2c531d95b21e953d8b
Chris <itzchwis@gmail.com>
2026-03-15 21:32:27 -0500

    Update lynx.h

--- lynx.h
+++ lynx.h
@@ -1,17 +1,37 @@
 #ifndef LYNX_H
 #define LYNX_H
 
-// 1. Token Types
+#include <stdbool.h>
+
+// Token Types
 typedef enum {
+    // Commands
     TOKEN_SET, TOKEN_ROAR, TOKEN_HUNT, TOKEN_STALK_PACK, TOKEN_HELP,
     TOKEN_POUNCE, TOKEN_IF, TOKEN_ELSE, TOKEN_LOAD_LIB,
+    
+    // New: Functions and Loops
+    TOKEN_FUNC, TOKEN_RETURN, TOKEN_FOR, TOKEN_WHILE, TOKEN_BREAK, TOKEN_CONTINUE,
+    
+    // New: Logic and Array
+    TOKEN_AND, TOKEN_OR, TOKEN_NOT, TOKEN_LBRACKET, TOKEN_RBRACKET,
+    
+    // Identifiers and Values
     TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
-    TOKEN_EQUAL, TOKEN_PLUS, TOKEN_MINUS, TOKEN_STAR, TOKEN_SLASH,
+    
+    // Operators
+    TOKEN_EQUAL, TOKEN_PLUS, TOKEN_MINUS, TOKEN_STAR, TOKEN_SLASH, TOKEN_MODULO,
+    TOKEN_INCREMENT, TOKEN_DECREMENT,
     TOKEN_EQ, TOKEN_NE, TOKEN_GT, TOKEN_LT, TOKEN_GE, TOKEN_LE,
-    TOKEN_LBRACE, TOKEN_RBRACE,
+    
+    // Delimiters
+    TOKEN_LBRACE, TOKEN_RBRACE, TOKEN_LPAREN, TOKEN_RPAREN,
+    TOKEN_COMMA, TOKEN_COLON,
+    
+    // End
     TOKEN_EOF, TOKEN_ERROR
 } LynxTokenType;
 
+// Token Structure
 typedef struct {
     LynxTokenType type;
     const char* start;
@@ -19,33 +39,65 @@ typedef struct {
     int line;
 } Token;
 
-// 2. Scanner State Structure
+// Scanner State
 typedef struct {
     const char* start;
     const char* current;
     int line;
 } Scanner;
 
-// 3. Global Scanner Declaration
+// Variable Type - now supports strings and numbers
+typedef enum {
+    VAR_NUMBER,
+    VAR_STRING,
+    VAR_ARRAY
+} VarType;
+
+typedef struct {
+    char name[64];
+    VarType type;
+    union {
+        double numValue;
+        char* strValue;
+    } value;
+} Variable;
+
+// Function Definition
+typedef struct {
+    char name[64];
+    char params[10][64];
+    int paramCount;
+    char* body;
+} Function;
+
+// Global Scanner
 extern Scanner scanner;
 
-// 4. Function Prototypes
+// Scanner Functions
 void initScanner(const char* source);
 Token scanToken();
 Token peekToken();
 
+// Parser Functions
 void parse_statement();
 void parse_block();
 double parse_expression();
 int check_condition();
 void runFile(const char* path);
 
+// Variable Functions
 void setVar(const char* name, double value);
+void setVarString(const char* name, const char* value);
 double getVar(const char* name);
+char* getVarString(const char* name);
 void pounce(const char* name);
 void hunt();
 
-// Library loading
+// Function Functions
+void defineFunction(const char* name, const char** params, int paramCount, const char* body);
+int callFunction(const char* name);
+
+// Library Loading
 void load_lib(const char* lib_name);
 void unload_all_libs();
 
